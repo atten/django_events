173e0fb1c01ea987d5a117a3ddab4f2e09d55ed7
night-crawler <lilo.panic@gmail.com>
2017-12-16 21:35:11 +0300

    Bumped version: 1.1.1 → 1.1.2

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -2,7 +2,7 @@
 
 docker build \
     -t msa_events:latest \
-    -t msa_events:1.1.1 \
+    -t msa_events:1.1.2 \
     -t docker.force.fm/msa/msa_events:latest \
-    -t docker.force.fm/msa/msa_events:1.1.1 \
+    -t docker.force.fm/msa/msa_events:1.1.2 \
     .
